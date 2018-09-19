37c966debafb49c4210de79068339db89712e5f6
Stacey Watro <watro.stacey@gmail.com>
2018-09-19 12:09:37 -0400

    Add functionality to select states. Excludes states not visited.

--- src/map.js
+++ src/map.js
@@ -1,21 +1,36 @@
 
 var dataProvider = {
     mapVar: AmCharts.maps.usaLow,
-    getAreasFromMap:true
+    getAreasFromMap: true, 
+    areas : [
+      {
+        id: "US-OK",
+        selectable: false
+      }, 
+      {
+      	id: "US-KS",
+        selectable: false
+      },
+	  {
+      	id: "US-NE",
+        selectable: false
+      }
+    ]
 };
 
 var map;
 AmCharts.ready(function() {
     map = new AmCharts.AmMap();
-
     map.dataProvider = dataProvider;
     map.balloon.color = "#000000";
     map.dragMap = false;
     map.backgroundColor = "#ffffff";
+    map.zoomOnDoubleClick = false;
 
     map.areasSettings = {
         autoZoom: false,
-        selectedColor: "#CC0000"
+        selectedColor: "#CC0000",
+        selectable: true
     };
 
     map.zoomControl = {
@@ -23,6 +38,9 @@ AmCharts.ready(function() {
     	zoomControlEnabled: false
     }
 
+    map.addListener('clickMapObject', function(event){
+    	console.log("event")
+    })
     map.write("mapdiv");
 
 });
